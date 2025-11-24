972c09fafc32df521352c1ee9b4db4d7ac2f6c1e
digamber22 <digamberbosak@gmail.com>
2025-11-24 19:31:54 +0530

    fix: allow all origins in CORS config

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,10 +18,7 @@ const io = connectToSocket(server);
 
 
 const port = process.env.PORT || 8000;
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
