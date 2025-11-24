e5361a3c4f271e3983df45b17b3d3229892b7a87
digamber22 <digamberbosak@gmail.com>
2025-11-24 18:24:06 +0530

    feat: add backend structure

---
+++ backend/src/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { createServer } from "node:http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+const port = process.env.PORT || 8000;
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.get('/api/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    const connectionDb = await mongoose.connect(process.env.MONGO_URI)
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(port, () => {
+        console.log(`LISTENING ON PORT ${port}`)
+    });
+
+
+
+}
+
+
+
+start();
